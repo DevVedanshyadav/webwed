f2183fa08f979d81eee02537b61e5a0790439f84
VDNSH <129854507+DevVedanshyadav@users.noreply.github.com>
2026-01-27 14:06:19 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -10,8 +10,17 @@ const floating = gsap.to(
 let env_open = false
 
 const flap = document.querySelector(".flap")
+const letter = document.querySelector(".letter")
+
 const revealtimeline = gsap.timeline()
 
+gsap.to("body", {
+    "--deg":"360",
+    duration: 8,
+    repeat: -1,
+    yoyo: true,
+    ease: "power2.inOut"
+});
 
 flap.addEventListener("click", () => {
     floating.pause()
@@ -19,13 +28,24 @@ flap.addEventListener("click", () => {
 
     gsap.to(".flap", { scaleY: -1, duration: 1, y: 0, ease: "power2.inOut" })
     const revealAni = revealtimeline
-                        .to(".letter",{zIndex:4,duration:2,y:-100})
-                        .to(".letter",{scaleY:4,scaleX:2,duration:1,y:20})
+        .to(".letter", { zIndex: 4, duration: 2, y: -100 })
+        .to(".letter", { scaleY: 4, scaleX: 2, duration: 1, y: 20 })
 
 
 })
+let n_ = 1
+letter.addEventListener("click", () => {
+    n_++;
+    if (n_ == 4) {
+        n_ = 1;
+    }
 
-
+    letter.src = `${n_}.png`
+    gsap.to(".letter", {
+        scaleY: 0.5, scale: 0.5
+    })
+    gsap.to(".letter", { scaleY: 4, scaleX: 2 })
+})
 
 if (!env_open) {
     floating.play()
